9c49524c597d4026c0a729a776470b9a966408c2
Abdullah Gamal <abdullahgamal661@gmail.com>
2026-08-23 13:23:48 +0300

    init.c done

--- init.c
+++ init.c
@@ -7,15 +7,25 @@ void ADC_Init(void)
     SET_BIT(RCC_APB2ENR,8) ;
 
     // Configure ADC clock
-
-
-
+    CLEAR_BIT(ADC_CCR,16);
+    CLEAR_BIT(ADC_CCR,17);
+    SET_BIT(ADC_CCR,16);
 
     // Configure ADC resolution
+    CLEAR_BIT(ADC1_CR1,24);
+    CLEAR_BIT(ADC1_CR1,25);
 
     // Configure sampling time
+    CLEAR_BIT(ADC1_SMPR2,0) ;
+    CLEAR_BIT(ADC1_SMPR2,1) ;
+    SET_BIT(ADC1_SMPR2,2) ;
 
     // Select channel
+    CLEAR_BIT(ADC1_SQR3,0);
+    CLEAR_BIT(ADC1_SQR3,1);
+    CLEAR_BIT(ADC1_SQR3,2);
+    CLEAR_BIT(ADC1_SQR3,3);
+    CLEAR_BIT(ADC1_SQR3,4);
 
     // Number of conversions
     CLEAR_BIT(ADC1_SQR1 ,20) ;
@@ -25,6 +35,9 @@ void ADC_Init(void)
 
     // Enable ADC
     SET_BIT(ADC1_CR2,0) ;
+
+    // cont. mode
+    SET_BIT(ADC1_CR2,1);
 }
 
 
--- private.h
+++ private.h
@@ -1,14 +1,14 @@
 #ifndef PRIVATE_H
 #define PRIVATE_H
 
-#define ADC1_BASE_ADDRESS    0x40012000
+#define ADC1_BASE_ADDRESS 0x40012000
 
-#define ADC1_CR1             (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x04))
-#define ADC1_CR2             (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x08))
-#define ADC1_SMPR2           (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x10))
-#define ADC1_SQR1            (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x2C))
-#define ADC1_SQR3            (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x34))
-#define RCC_APB2ENR          (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x44))
-
+#define ADC1_CR1 (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x04))
+#define ADC1_CR2 (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x08))
+#define ADC1_SMPR2 (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x10))
+#define ADC1_SQR1 (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x2C))
+#define ADC1_SQR3 (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x34))
+#define RCC_APB2ENR (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x44))
+#define ADC_CCR (*(volatile unsigned int *)(ADC1_BASE_ADDRESS + 0x04))
 
 #endif
